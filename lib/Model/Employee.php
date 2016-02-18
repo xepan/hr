164468ae262ea9cebf1faaf850da90c27846914f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-18 10:48:05 +0530

    Not joined with user but hasOne User now

--- lib/Model/Employee.php
+++ lib/Model/Employee.php
@@ -9,15 +9,14 @@ class Model_Employee extends \xepan\base\Model_Contact{
 
 		$emp_j = $this->join('employee.contact_id');
 
+		$emp_j->hasOne('xepan\base\User');
 		$emp_j->hasOne('xepan\hr\Post');
 		$emp_j->addField('is_active')->type('boolean')->defaultValue(true);
 
-		$user_j = $this->join('user.contact_id');
-		$user_j->addField('username');
-		$user_j->addField('password');
-		$user_j->addField('user_id','id');
 
 		$this->addCondition('type','employee');
+
+		$this->debug();
 
 	}
 }
