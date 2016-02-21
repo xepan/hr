3a5f8f27137ff3c8f4212078e499cc2b4f74a979
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-21 22:36:12 +0530

    Should not extend Document .. still unclear

--- lib/Model/Post.php
+++ lib/Model/Post.php
@@ -2,15 +2,16 @@
 
 namespace xepan\hr;
 
-class Model_Post extends \xepan\base\Model_Document{
+class Model_Post extends \xepan\base\Model_Table{
 	public $table="post";
 	
 	function init(){
 		parent::init();
 
+		$this->hasOne('xepan\base\Epan');
+
 		$this->hasOne('xepan\hr\Department');
 		$this->addField('name');
-		// $this->addField('status')->enum(['Active','DeActive']);
 
 		$this->hasMany('xepan\hr\Employee',null,null,'Employees');
 
