203cc60a03bdd9a0c107433081b2cfc4a82c289d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-02-24 21:24:54 +0530

    ACL Base looks ready ;)

--- lib/Model/ACL.php
+++ lib/Model/ACL.php
@@ -20,10 +20,10 @@ class Model_ACL extends \xepan\base\Model_Table {
 		$this->hasOne('xepan\base\Epan','epan_id');
 		$this->hasOne('xepan\hr\Post','post_id');
 		$this->addField('document_type');
-		$this->addField('status');
 		$this->addField('action_allowed')->type('text')->defaultValue(json_encode([]));
+		$this->addField('allow_add')->type('boolean')->defaultValue(true);
 
-		$this->addHook('afterLook',function($m){
+		$this->addHook('afterLoad',function($m){
 			$m['action_allowed'] = json_decode($m['action_allowed'],true);
 		});
 	}
