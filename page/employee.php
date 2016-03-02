05a80bc6f8429427c73c2d0ba200fdeaf466c5a1
vijay mali <guddavijay@gmail.com>
2016-03-02 18:34:51 +0530

    Employee Page Filter added

--- page/employee.php
+++ page/employee.php
@@ -29,13 +29,32 @@ class page_employee extends \Page {
 		$crud->setModel($employee,['first_name','last_name','post','created_at','status','email']);
 		$f = $crud->grid->addQuickSearch(['first_name','last_name']);
 
-		$d_f =$f->addField('DropDown','department_id')->setEmptyText("All");
+		$d_f =$f->addField('DropDown','department_id')->setEmptyText("Select Department");
 		$d_f->setModel('xepan\hr\Department');
 		$d_f->js('change',$f->js()->submit());
+		
+		$p_f =$f->addField('DropDown','post_id')->setEmptyText("Select Post");
+		$p_f->setModel('xepan\hr\Post');
+		$p_f->js('change',$f->js()->submit());
+
+		$s_f=$f->addField('DropDown','status')->setValueList(['Active'=>'Active','Inactive'=>'Inactive'])->setEmptyText('Status');
+		$s_f->js('change',$f->js()->submit());
 
 		$f->addHook('appyFilter',function($f,$m){
 			if($f['department_id'])
 				$m->addCondition('department_id',$f['department_id']);
+			if($f['post_id'])
+				$m->addCondition('post_id',$f['post_id']);
+			
+			if($f['status']='Active'){
+				throw new \Exception("Active", 1);
+				$m->addCondition('status','Active');
+			}else{
+				throw new \Exception("Inactive", 1);
+				$m->addCondition('status','Inactive');
+
+			}
+
 		});
 		
 	}
