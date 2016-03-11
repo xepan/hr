3fb6718713e83430388a1ca1afa3c1362784f8c7
vijay mali <guddavijay@gmail.com>
2016-03-11 16:03:34 +0530

    Employee add Official Details

--- lib/Model/Employee.php
+++ lib/Model/Employee.php
@@ -11,14 +11,18 @@ class Model_Employee extends \xepan\base\Model_Contact{
 
 	function init(){
 		parent::init();
-
+		$this->getElement('post')->destroy();
 		$emp_j = $this->join('employee.contact_id');
 
-		$emp_j->hasOne('xepan\base\User');
+		$emp_j->hasOne('xepan\base\User',null,'username');
 		$emp_j->hasOne('xepan\hr\Department','department_id');
 		$emp_j->hasOne('xepan\hr\Post','post_id');
 
 		$emp_j->addField('notified_till')->type('number')->defaultValue(0); // TODO Should be current id of Activity
+		$emp_j->addField('offer_date')->type('date');
+		$emp_j->addField('doj')->caption('Date of Joining')->type('date');
+		$emp_j->addField('contract_date')->type('date');
+		$emp_j->addField('leving_date')->type('date');
 
 		$emp_j->hasMany('xepan\hr\Qualification','employee_id',null,'Qualifications');
 		$emp_j->hasMany('xepan\hr\Experience','employee_id',null,'Experiences');
