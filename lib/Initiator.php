7aea77fc27de298745dfcd037c06025b856b0ed6
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-18 14:58:08 +0530

    Updated generateINstaller for single Running .. not when new epan created

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -41,6 +41,10 @@ class Initiator extends \Controller_Addon {
 
 	function generateInstaller(){
         // Clear DB
+        
+        if(!isset($this->app->old_epan)) $this->app->old_epan = $this->app->epan;
+        if(!isset($this->app->new_epan)) $this->app->new_epan = $this->app->epan;
+
         $this->app->epan=$this->app->old_epan;
         $truncate_tables = ['ACL','Activity','Employee','Post','Department'];
         foreach ($truncate_tables as $t) {
