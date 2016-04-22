ded797bc2d4d8730e94aa74eaa296e0c8b212f5d
dkanojia <dkanojia93@gmail.com>
2016-04-22 14:00:16 +0530

    icon added

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -22,9 +22,9 @@ class Initiator extends \Controller_Addon {
 			$m = $this->app->top_menu->addMenu('HR');
 			$m->addItem(['Department','icon'=>'fa fa-sliders'],'xepan_hr_department');
 			$m->addItem(['Post','icon'=>'fa fa-sitemap'],'xepan_hr_post');
-			$m->addItem(['Employee','icon'=>'fa fa-user'],'xepan_hr_employee');
-			$m->addItem(['Employee Movement','icon'=>'fa fa-edit'],'xepan_hr_employeemovement');
-			$m->addItem(['User','icon'=>'fa fa-male'],'xepan_hr_user');
+			$m->addItem(['Employee','icon'=>'fa fa-male'],'xepan_hr_employee');
+			$m->addItem(['Employee Movement','icon'=>'fa fa-eye'],'xepan_hr_employeemovement');
+			$m->addItem(['User','icon'=>'fa fa-user'],'xepan_hr_user');
 			$m->addItem(['ACL','icon'=>'fa fa-dashboard'],'xepan_hr_aclmanagement');
 			
 			$this->app->layout->template->trySet('department',$this->app->employee['department']);
