704d0d154cb5abb0aab5705cbc08b3e97f468484
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-28 11:29:44 +0530

    Only proceed if admin logged in

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -14,32 +14,28 @@ class Initiator extends \Controller_Addon {
 		->setBaseURL('../vendor/xepan/hr/');
 
 
-		if($this->app->is_admin){
+		if($this->app->is_admin && $this->app->auth->isLoggedIn()){
 
-			if($this->app->auth->isLoggedIn())
-				$this->app->employee = $this->add('xepan\hr\Model_Employee')->loadBy('user_id',$this->app->auth->model->id);
-
-			$m = $this->app->top_menu->addMenu('HR');
-			$m->addItem(['Department','icon'=>'fa fa-sliders'],'xepan_hr_department');
-			$m->addItem(['Post','icon'=>'fa fa-sitemap'],'xepan_hr_post');
-			$m->addItem(['Employee','icon'=>'fa fa-male'],'xepan_hr_employee');
-			$m->addItem(['Employee Movement','icon'=>'fa fa-eye'],'xepan_hr_employeemovement');
-			$m->addItem(['User','icon'=>'fa fa-user'],'xepan_hr_user');
-			$m->addItem(['ACL','icon'=>'fa fa-dashboard'],'xepan_hr_aclmanagement');
-			
-			$this->app->layout->template->trySet('department',$this->app->employee['department']);
-			$post=$this->app->employee->ref('post_id');
-	        $this->app->layout->template->trySet('post',$post['name']);
-	        $this->app->layout->template->trySet('first_name',$this->app->employee['first_name']);
-	        $this->app->layout->template->trySet('status',$this->app->employee['status']);
-	        
-	        if($user_loggedin = $this->app->recall('user_loggedin',false)){
+            $m = $this->app->top_menu->addMenu('HR');
+            $m->addItem(['Department','icon'=>'fa fa-sliders'],'xepan_hr_department');
+            $m->addItem(['Post','icon'=>'fa fa-sitemap'],'xepan_hr_post');
+            $m->addItem(['Employee','icon'=>'fa fa-male'],'xepan_hr_employee');
+            $m->addItem(['Employee Movement','icon'=>'fa fa-eye'],'xepan_hr_employeemovement');
+            $m->addItem(['User','icon'=>'fa fa-user'],'xepan_hr_user');
+            $m->addItem(['ACL','icon'=>'fa fa-dashboard'],'xepan_hr_aclmanagement');
+            
+    		$this->app->employee = $this->add('xepan\hr\Model_Employee')->loadBy('user_id',$this->app->auth->model->id);
+            $this->app->layout->template->trySet('department',$this->app->employee['department']);
+            $post=$this->app->employee->ref('post_id');
+            $this->app->layout->template->trySet('post',$post['name']);
+            $this->app->layout->template->trySet('first_name',$this->app->employee['first_name']);
+            $this->app->layout->template->trySet('status',$this->app->employee['status']);
+            
+            if($user_loggedin = $this->app->recall('user_loggedin',false)){
                 $this->app->forget('user_loggedin');
                 $this->api->employee->afterLoginCheck();
             }
-
             $this->app->layout->add('xepan\hr\View_Notification',null,'notification_view');
-            // $this->app->layout->add('xepan\base\View_Message',null,'message_view');
 
 		}else{
             $this->app->employee = $this->add('xepan\hr\Model_Employee')
