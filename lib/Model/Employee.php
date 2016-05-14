80ee93a55c137fadd670c8dfb70a40eb70f285fe
vaibhav <vaibhavsharma38@gmail.com>
2016-05-14 11:31:33 +0530

    quick search functionality added in hr

--- lib/Model/Employee.php
+++ lib/Model/Employee.php
@@ -162,6 +162,8 @@ class Model_Employee extends \xepan\base\Model_Contact{
 		$search_string .=" ". $this['mode'];
 		$search_string .=" ". $this['in_time'];
 		$search_string .=" ". $this['out_time'];
+		$search_string .=" ". $this['first_name'];
+		$search_string .=" ". $this['last_name'];
 
 		$qualification = $this->ref('Qualifications');
 		foreach ($qualification as $qualification_detail) {
