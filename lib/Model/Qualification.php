817d96c28cd656e608e0a1cdc9df949f67133cd8
dkanojia <dkanojia93@gmail.com>
2016-05-14 15:06:06 +0530

    hint added in model employee

--- lib/Model/Qualification.php
+++ lib/Model/Qualification.php
@@ -8,8 +8,8 @@ class Model_Qualification extends \xepan\base\Model_Table{
 	function init(){
 		parent::init();
 		$this->hasOne('xepan\hr\Employee','employee_id');
-		$this->addField('name')->caption('Qualification');
-		$this->addField('qualificaton_level');
+		$this->addField('name')->caption('Qualification')->hint('Name of Degree/Course');
+		$this->addField('qualificaton_level')->hint('Graduation/Master/Phd/Others');
 		$this->addField('remarks')->type('text');
 
 	}
