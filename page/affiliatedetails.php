57cbc771f1f52ce3daf6ec8f2b8f524c2a40ceb2
dkanojia <dkanojia93@gmail.com>
2016-06-13 17:10:42 +0530

    contact view detail

--- page/affiliatedetails.php
+++ page/affiliatedetails.php
@@ -12,7 +12,18 @@ class page_affiliatedetails extends \xepan\base\Page {
 
 		$action = $this->api->stickyGET('action')?:'view';
 		$affiliate= $this->add('xepan\hr\Model_Affiliate')->tryLoadBy('id',$this->api->stickyGET('contact_id'));
-		$affiliate_view = $this->add('xepan\base\View_Contact',['acl'=>'xepan\hr\Model_affiliate'],'contact_view');
+		
+		if($action=="add"){
+
+			$affiliate_view = $this->add('xepan\base\View_Contact',['acl'=>'xepan\hr\Model_Affiliate','view_document_class'=>'xepan\hr\View_Document'],'contact_view_full_width');
+			$affiliate_view->document_view->effective_template->del('im_and_events_andrelation');
+			$affiliate_view->document_view->effective_template->del('email_and_phone');
+			$this->template->del('other_details');
+			$affiliate_view->setStyle(['width'=>'50%','margin'=>'auto']);
+		}else{
+			$affiliate_view = $this->add('xepan\base\View_Contact',['acl'=>'xepan\hr\Model_Affiliate','view_document_class'=>'xepan\hr\View_Document'],'contact_view');
+		}	
+
 		$affiliate_view->setModel($affiliate);
 
 		$detail = $this->add('xepan\hr\View_Document',['action'=> $action,'id_field_on_reload'=>'contact_id'],'details',['view/affiliate/details']);
