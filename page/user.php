4317a263b2a7584f7cd45959c9b07dd85770e3de
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-06-14 09:53:16 +0530

    added scope in fecth field of grid

--- page/user.php
+++ page/user.php
@@ -21,7 +21,7 @@ class page_user extends \xepan\base\Page{
 		$user_view->grid->addPaginator(50);
 		$user_view->grid->addQuickSearch(['username']);
 		$user_view->add('xepan\base\Controller_Avatar',['options'=>['size'=>50,'border'=>['width'=>0]],'name_field'=>'username','default_value'=>'']);		
-		$user_view->setModel($user_m,array('created_by_id','username','password','type','scope','status'),array('username','type','status','action','related_contact','related_contact_type'));
+		$user_view->setModel($user_m,array('created_by_id','username','password','type','scope','status'),array('username','type','status','action','related_contact','related_contact_type','scope'));
 
 	}
 }
