3b36398cdddd88735ca08c7a0a5f7bc2cf1ca01b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-07-02 16:31:41 +0530

    Multiple status now allowed in ACL

--- lib/Model/Activity.php
+++ lib/Model/Activity.php
@@ -13,7 +13,7 @@ namespace xepan\hr;
 
 class Model_Activity extends \xepan\base\Model_Activity{
 
-	function notifyWhoCan($list_of_actions,$current_status,$model=null,$notify_self=true){
+	function notifyWhoCan($list_of_actions,$current_statuses,$model=null,$notify_self=true){
 		$acl_m = $this->add('xepan\hr\Model_ACL');
 		if(!$model)
 			$model = $this->ref('related_document_id');
@@ -21,35 +21,36 @@ class Model_Activity extends \xepan\base\Model_Activity{
 		$acl_m->addCondition('document_type',$model['type']);
 	
 		if(!is_array($list_of_actions)) $list_of_actions = explode(",", $list_of_actions);		
+		if(!is_array($current_statuses)) $current_statuses = explode(",", $current_statuses);		
 
 		
 		$employee_ids=[];
 		foreach ($acl_m as $acl) {
-			$actions = $acl['action_allowed'][$current_status];
-
-			foreach ($list_of_actions as $req_act) {
-				$text_code = $actions[$req_act]; // Self Only, All, None, Etc.
-				switch ($text_code) {
-					case 'Self Only':
-						# if($model->created_by->post_id == $acl->post_id) include this id
-						if($this->getPost($model['created_by_id'])->get('id') == $acl['post_id'])
-							$employee_ids[] = $model['created_by_id'];
-						break;
-					case 'All':
-						# code...
-						# include all employees under $acl->post_id
-						foreach ($acl->ref('post_id')->ref('Employees')->addCondition('status','Active') as $emp) {
-							$employee_ids [] = $emp->id;
-						}
-						break;
-					case 'None':
-						# code...
-						break;
-					default:
-						# code...
-						break;
+			foreach ($current_statuses as $current_status) {
+				$actions = $acl['action_allowed'][$current_status];
+				foreach ($list_of_actions as $req_act) {
+					$text_code = $actions[$req_act]; // Self Only, All, None, Etc.
+					switch ($text_code) {
+						case 'Self Only':
+							# if($model->created_by->post_id == $acl->post_id) include this id
+							if($this->getPost($model['created_by_id'])->get('id') == $acl['post_id'])
+								$employee_ids[] = $model['created_by_id'];
+							break;
+						case 'All':
+							# code...
+							# include all employees under $acl->post_id
+							foreach ($acl->ref('post_id')->ref('Employees')->addCondition('status','Active') as $emp) {
+								$employee_ids [] = $emp->id;
+							}
+							break;
+						case 'None':
+							# code...
+							break;
+						default:
+							# code...
+							break;
+					}
 				}
-
 			}
 		}
 		
