b0bb65dbdf92aae431942dac634ef0d025394dd3
dkanojia <dkanojia93@gmail.com>
2016-07-21 18:02:37 +0530

    activities

--- page/activity.php
+++ page/activity.php
@@ -7,14 +7,6 @@ class page_activity extends \xepan\base\Page{
 	function init(){
 		parent::init();
 
-		$activity=$this->add('xepan\hr\Model_Activity');
-		// $activity->addCondition('notify_to','like','%"'.$this->app->employee->id.'"%');
-		
-		$g=$this->add('xepan\hr\Grid',null,null,['view/activity/activities-info']);
-		$g->setModel($activity);
-
-
-		$g->addPaginator(25);
-
+		$this->add('xepan\base\View_Activity');
 	}
 }
