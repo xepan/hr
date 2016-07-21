6831cd329f2230c3d89077067f374da7e5b41d57
dkanojia <dkanojia93@gmail.com>
2016-07-21 13:07:55 +0530

    changes

--- page/activity.php
+++ page/activity.php
@@ -13,8 +13,6 @@ class page_activity extends \xepan\base\Page{
 		$g=$this->add('xepan\hr\Grid',null,null,['view/activity/activities-info']);
 		$g->setModel($activity);
 
-		$crud->grid->js('click')->_selector('.do-view-customer-detail')->univ()->frameURL('Customer Details',[$this->api->url('xepan_commerce_customerdetail'),'contact_id'=>$this->js()->_selectorThis()->closest('[data-customer-id]')->data('id')]);
-		}
 
 		$g->addPaginator(25);
 
