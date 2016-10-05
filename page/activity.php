817d6be9b2ce1060601402038d7ab4c984c28b25
sinharakesh <rksinha.btech@gmail.com>
2016-10-05 19:24:07 +0530

    activity filter added

--- page/activity.php
+++ page/activity.php
@@ -7,6 +7,30 @@ class page_activity extends \xepan\base\Page{
 	function init(){
 		parent::init();
 
-		$this->add('xepan\base\View_Activity');
+		$this->app->stickyGET('from_date');
+		$this->app->stickyGET('to_date');
+		$this->app->stickyGET('contact_id');
+
+		$custom_date = strtotime(date("Y-m-d", strtotime('-1 month', strtotime($this->app->today))));
+
+		$form = $this->add('Form');
+		$form->addField('DatePicker','from_date')->validate('required')->set($custom_date);
+		$form->addField('DatePicker','to_date')->validate('required')->set($this->app->today);
+		$form->addField('xepan\base\Basic','contact')->setModel($this->add('xepan\base\Model_Contact'));
+
+		$form->addSubmit("Filter");
+
+		$activity_view = $this->add('xepan\base\View_Activity',['from_date'=>$_GET['from_date'],'to_date'=>$_GET['to_date'],'contact_id'=>$_GET['contact_id']]);
+
+		if($form->isSubmitted()){
+			$form->js(null,$activity_view->js()
+											->reload(
+												[
+													'from_date'=>$form['from_date']?:0,
+													'to_date'=>$form['to_date']?:0,
+													'contact_id'=>$form['contact']?:0
+												]))->univ()->successMessage('wait ... ')->execute();
+
+		}
 	}
 }
