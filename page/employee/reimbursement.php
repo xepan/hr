e5e7a005044aa788f31124d069327f748eba35e0
sinharakesh <rksinha.btech@gmail.com>
2016-10-25 18:41:29 +0530

    official_holiday, reimbursement and working_week_day added

---
+++ page/employee/reimbursement.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace xepan\hr;
+
+class page_employee_reimbursement extends \xepan\hr\page_employee_myhr{
+	public $title="My Reimbursement";
+	function init(){
+		parent::init();
+
+
+		$crud = $this->add('xepan\hr\CRUD',null,null,['view/employee/reimbursement']);
+
+		$model = $this->add('xepan\hr\Model_Reimbursement');
+		$model->setOrder('created_at','desc');
+
+		$crud->setModel($model);
+		$crud->addRef('Details',[
+									'view_class'=>"xepan\base\CRUD",
+									'label'=>"Details",
+									'fields'=>['name','date','narration','amount']
+								]);
+	}
+}
