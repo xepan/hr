37374044217f24b04e5cae3ff48c6db5f2bf1f9f
sinharakesh <rksinha.btech@gmail.com>
2016-10-26 11:23:00 +0530

    my or hr reimbursement page added

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -23,6 +23,7 @@ class Initiator extends \Controller_Addon {
             $m->addItem(['Employee Attandance','icon'=>'fa fa-check-square-o'],'xepan_hr_employeeattandance');
             $m->addItem(['Employee Movement','icon'=>'fa fa-eye'],'xepan_hr_employeemovement');
             $m->addItem(['Leave Management','icon'=>'fa fa-eye'],'xepan_hr_leavemanagment');
+            $m->addItem(['Reimbursement Management','icon'=>'fa fa-money'],'xepan_hr_reimbursement');
             // $m->addItem(['Payroll','icon'=>'fa fa-money'],'xepan_hr_payroll');
             $m->addItem(['User','icon'=>'fa fa-user'],$this->app->url('xepan_hr_user',['status'=>'Active']));
             $m->addItem(['Affiliate','icon'=>'fa fa-user'],$this->app->url('xepan_hr_affiliate',['status'=>'Active']));
