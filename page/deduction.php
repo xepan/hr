b311cbbed81457b48672f375b127295b4d750029
sinharakesh <rksinha.btech@gmail.com>
2016-10-26 16:46:55 +0530

    hr deduction crud added

---
+++ page/deduction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace xepan\hr;
+
+class page_deduction extends \xepan\base\Page{
+	public $title = "Deduction Management";
+
+	function init(){
+		parent::init();
+
+		$crud = $this->add('xepan\hr\CRUD',null,null,['view/deduction']);
+
+		$model = $this->add('xepan\hr\Model_Deduction');
+		// $model->setOrder('created_at','desc');
+		$crud->setModel($model);
+	}
+}
