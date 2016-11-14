457ebd76e3f199ce656d1ee9c539ddaa3b638750
dkanojia <dkanojia93@gmail.com>
2016-11-14 18:32:54 +0530

    builder page updated

--- page/graphicalreport/builder.php
+++ page/graphicalreport/builder.php
@@ -26,7 +26,36 @@ class page_graphicalreport_builder extends \xepan\base\Page {
 		$c = $this->add('xepan\hr\CRUD',null,null,['view\graphicalreportbuilder']);
 		$c->setModel($m,['name']);
 		if(!$c->isEditing()){
+			$import_btn=$c->grid->addButton('import')->addClass('btn btn-primary');
+
+			$p=$this->add('VirtualPage');
+			$p->set(function($p){
+				$f=$p->add('Form');
+				$f->addField('text','json');
+				$f->addSubmit('Go');
+				
+				if($f->isSubmitted()){
+					$import_m=$this->add('xepan\base\Model_GraphicalReport');
+
+					$import_m->importJson($f['json']);	
+					
+					$f->js()->reload()->univ()->successMessage('Done')->execute();
+				}
+			});
+			if($import_btn->isClicked()){
+				$this->js()->univ()->frameURL('Import',$p->getUrl())->execute();
+			}
+
 			$c->grid->addColumn('link','run')->setTemplate('<a href="'.$this->app->url('xepan/hr/graphicalreport/runner',[])->getURL().'&report_id={$id}">{$name}</a>');
+			
+			$p=$this->add('VirtualPage');
+			$p->set(function($p){
+					$export_m=$this->add('xepan\base\Model_GraphicalReport')->load($p->id);
+					$json=$export_m->exportJson();
+					$p->add('View')->set($json);
+			});
+
+			$p->addColumn("export", "export", "export", $c->grid);
 		}
 	}
 }
