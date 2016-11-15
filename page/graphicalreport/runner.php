c981982c30b831f739846e60002c5ff71bb4e643
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-11-15 19:42:15 +0530

    Set Empty Text on fields of dropdown type

--- page/graphicalreport/runner.php
+++ page/graphicalreport/runner.php
@@ -65,8 +65,11 @@ class page_graphicalreport_runner extends \xepan\base\Page {
 
 		$fld = $this->filter_form->addField($this->entity_list[$filter_entity]['type'],$filter_entity,$this->entity_list[$filter_entity]['caption']?:null);
 		
-		if($this->entity_list[$filter_entity]['model'])
+		if($this->entity_list[$filter_entity]['model']){
 			$fld->setModel($this->entity_list[$filter_entity]['model']);
+			if($fld->hasMethod('setEmptyText'))
+                $fld->setEmptyText('Please select');
+		}
 		
 		if(isset($this->$filter_entity))
 			$fld->set($this->$filter_entity);
