237c2e63dfbc078ab517d62f4a4e4217cc1ff6da
vaibhav <vaibhavsharma38@gmail.com>
2016-11-15 14:22:22 +0530

    individual report json added

--- lib/Widget/MyCommunication.php
+++ lib/Widget/MyCommunication.php
@@ -7,7 +7,7 @@ class Widget_MyCommunication extends \xepan\base\Widget{
 		parent::init();
 
 		$this->report->enableFilterEntity('date_range');
-		$this->grid = $this->add('xepan\hr\CRUD',['allow_add'=>null,'grid_class'=>'xepan\communication\View_Widget_CommunicationLister']);	    
+		$this->grid = $this->add('xepan\hr\Grid');
 	}
 
 	function recursiveRender(){
@@ -15,13 +15,15 @@ class Widget_MyCommunication extends \xepan\base\Widget{
 		$communication_model = $this->add('xepan\communication\Model_Communication');
 		$communication_model->addExpression('date','date(created_at)');
 		$communication_model->addCondition([['from_id',$this->app->employee->id],['to_id',$this->app->employee->id]]);
+		$communication_model->addCondition('communication_type','<>',['Newsletter','ReminderEmail']);
+		
 		if(isset($this->report->start_date))
 			$communication_model->addCondition('created_at','>',$this->report->start_date);
 		if(isset($this->report->end_date))
 			$communication_model->addCondition('created_at','<',$this->app->nextDate($this->report->end_date));
 		
-	 	$this->grid->setModel($communication_model);
-
+		$this->grid->setModel($communication_model,['to','title','communication_type','created_at']);
+		$this->grid->addPaginator(10);
 
 		return parent::recursiveRender();
 	}
