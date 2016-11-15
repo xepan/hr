f8177ea563487776f21442d332c2bc4792a7fed1
vaibhav <vaibhavsharma38@gmail.com>
2016-11-15 16:51:02 +0530

    looking of employee movenent changed

--- lib/Widget/EmployeeMovement.php
+++ lib/Widget/EmployeeMovement.php
@@ -12,16 +12,27 @@ class Widget_EmployeeMovement extends \xepan\base\Widget {
 
 	function recursiveRender(){
 		$employee = $this->add('xepan\hr\Model_Widget_EmployeeMovement');
-		
+
 		$this->grid->setModel($employee,['name','first_in','last_out','is_late','in_color','out_color']);
 		$this->grid->addPaginator(10);
 		
 		$this->grid->addHook('formatRow',function($g){
-			if($g->model['is_late'] || $g->model['first_in']== null)
-				$g->current_row_html['icon'] = 'fa fa-thumbs-o-down';
-			else
-				$g->current_row_html['icon'] = 'fa fa-thumbs-o-up';
+			if($g->model['first_in']== null)
+				$g->current_row_html['in_at'] = 'Not In';
+			else	
+				$g->current_row_html['in_at'] = date('h:i A', strtotime($g->model['first_in']));
+			
+			if($g->model['is_late'] || $g->model['first_in']== null){
+				$g->current_row_html['icon-class'] = 'fa-thumbs-o-down red-bg';
+				$g->current_row_html['text-class'] = 'value red';
+			}
+			else{
+				$g->current_row_html['icon-class'] = 'fa-thumbs-o-up green-bg';
+				$g->current_row_html['text-class'] = 'value green';
+			}
 		});
+
+		$this->grid->add('xepan\base\Controller_Avatar',['name_field'=>'name','image_field'=>'image_id','extra_classes'=>'profile-img center-block','options'=>['size'=>50,'display'=>'block','margin'=>'auto'],'float'=>null,'model'=>$this->model]);
 
 		return parent::recursiveRender();
 	}
