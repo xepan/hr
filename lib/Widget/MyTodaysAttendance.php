eb7d8f09026cf8e94ab96fe4967d2dd70a9cc84f
vaibhav <vaibhavsharma38@gmail.com>
2016-11-28 12:34:34 +0530

    reports added

--- lib/Widget/MyTodaysAttendance.php
+++ lib/Widget/MyTodaysAttendance.php
@@ -6,8 +6,8 @@ class Widget_MyTodaysAttendance extends \xepan\base\Widget {
 	
 	function init(){
 		parent::init();
-
-		$this->report->enableFilterEntity('employee');
+        
+        $this->view = $this->add('xepan\base\View_Widget_SingleInfo');
 
 	}
 
@@ -18,37 +18,29 @@ class Widget_MyTodaysAttendance extends \xepan\base\Widget {
 		
 		$emp_model = $this->add('xepan\hr\Model_Employee');
 
-		if(isset($this->report->employee)){
-	        $attan_m->tryLoadBy('employee_id',$this->report->employee);
-	        $emp_model->load($this->report->employee);
-		}else{
-	        $attan_m->tryLoadBy('employee_id',$this->app->employee->id);
-	        $emp_model->load($this->app->employee->id);
-		}
+        $attan_m->tryLoadBy('employee_id',$this->app->employee->id);
+        $emp_model->load($this->app->employee->id);
 
 		$emp_name = $emp_model['name'];
 
-        $view = $this->add('xepan\base\View_Widget_SingleInfo');
-
 		if(!$attan_m->loaded()){
-			$view->setIcon('fa fa-thumbs-down')
+			$this->view->setIcon('fa fa-thumbs-down')
                     ->setHeading(strtoupper($emp_name).' IS NOT PRESENT')
                     ->setValue('-')
                     ->makeDanger()
                     ;
 		}else{
 	        if($attan_m['late_coming']>0){
-	        	$view->setIcon('fa fa-thumbs-down')
+	        	$this->view->setIcon('fa fa-thumbs-down')
                     ->setHeading(date('h:i A', strtotime($attan_m['from_date'])).' ! '.strtoupper($emp_name).' IS LATE BY ')
                     ->setValue($attan_m['late_coming'].' Minutes')
-                    ->makeDanger()
-                    ;
+                    ->makeDanger();
+
 	        }else{
-	        	$view->setIcon('fa fa-thumbs-up')
+	        	$this->view->setIcon('fa fa-thumbs-up')
                     ->setHeading(date('h:i A', strtotime($attan_m['from_date'])).' ! '.strtoupper($emp_name).' IS EARLY BY ')
                     ->setValue(abs($attan_m['late_coming']).' Minutes')
-                    ->makeSuccess()
-                    ;
+                    ->makeSuccess();
 	        }
 		}
 		return parent::recursiveRender();
