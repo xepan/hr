20a650a7e832726a3363fe48147fdf2a5c5ad4cf
vaibhav <vaibhavsharma38@gmail.com>
2016-11-28 18:53:46 +0530

    late coming widget changed

--- lib/Widget/DepartmentLateComing.php
+++ lib/Widget/DepartmentLateComing.php
@@ -7,33 +7,47 @@ class Widget_DepartmentLateComing extends \xepan\base\Widget{
 		parent::init();
 
 		$this->report->enableFilterEntity('department');
-		$this->chart = $this->add('xepan\base\View_Chart');
+		$this->view = $this->add('View',null,null,['view\multibox']);
 	}
 
 	function recursiveRender(){
 		$attendances = $this->add('xepan\hr\Model_Employee_Attandance');
-		$attendances->addExpression('employee_department')->set(function($m,$q){
-			return $this->add('xepan\hr\Model_Employee')
-						->addCondition('id',$m->getElement('employee_id'))
-						->setLimit(1)
-						->fieldQuery('department_id');	
+		$attendances->addExpression('emp_department')->set(function($m,$q){
+			$emp = $this->add('xepan\hr\Model_Employee');
+			$emp->addCondition('id',$m->getElement('employee_id'));
+			$emp->setLimit(1);
+			return $emp->fieldQuery('department_id');
 		});
 
-		if(isset($this->report->department)){
-			$attendances->addCondition('employee_department',$this->report->department);
+		$attendances->addExpression('emp_status')->set(function($m,$q){
+			$emp = $this->add('xepan\hr\Model_Employee');
+			$emp->addCondition('id',$m->getElement('employee_id'));
+			$emp->setLimit(1);
+			return $emp->fieldQuery('status');
+		});
+
+		$attendances->addCondition('emp_status','Active');
+
+		if(isset($this->report->department)){			
+			$attendances->addCondition('emp_department',$this->report->department);
 		}else{
-			$attendances->addCondition('employee_department',$this->app->employee['department_id']);
+			$attendances->addCondition('emp_department',$this->app->employee['department_id']);
 		}
-		
+
 		$attendances->addExpression('avg_late')->set($attendances->dsql()->expr('AVG([0])/60',[$attendances->getElement('late_coming')]));
 		$attendances->addExpression('avg_extra_work')->set($attendances->dsql()->expr('AVG([0])/60',[$attendances->getElement('extra_work')]));
 		$attendances->_dsql()->group('employee_id');
-     	
-		$this->chart->setType('bar')
-		    		->setModel($attendances,'employee',['avg_late','avg_extra_work'])
-		    		->rotateAxis()
-		    		->setTitle('Employee Avg Late Coming & Extra Work');
+
+		$total_avg_late = 0;
+		$total_extra_work = 0;
+		foreach ($attendances as $att){
+			$total_avg_late += $att['avg_late'];
+			$total_extra_work += $att['avg_extra_work'];
+		}
 		
+		$this->view->template->trySet('value1',$total_avg_late);
+		$this->view->template->trySet('value2',$total_extra_work);
+
 		return parent::recursiveRender();
 	}
 }
--- lib/Widget/LateComing.php
+++ lib/Widget/LateComing.php
@@ -6,19 +6,43 @@ class Widget_LateComing extends \xepan\base\Widget{
 	function init(){
 		parent::init();
 
-		$this->chart = $this->add('xepan\base\View_Chart');
+		$this->view = $this->add('View',null,null,['view\multibox']);
 	}
 
 	function recursiveRender(){
 		$attendances = $this->add('xepan\hr\Model_Employee_Attandance');
+
+		$attendances->addExpression('emp_status')->set(function($m,$q){
+			$emp = $this->add('xepan\hr\Model_Employee');
+			$emp->addCondition('id',$m->getElement('employee_id'));
+			$emp->setLimit(1);
+			return $emp->fieldQuery('status');
+		});
+
+		$attendances->addCondition('emp_status','Active');
+
 		$attendances->addExpression('avg_late')->set($attendances->dsql()->expr('AVG([0])/60',[$attendances->getElement('late_coming')]));
 		$attendances->addExpression('avg_extra_work')->set($attendances->dsql()->expr('AVG([0])/60',[$attendances->getElement('extra_work')]));
 		$attendances->_dsql()->group('employee_id');
+
+		$total_avg_late = 0;
+		$total_extra_work = 0;
+		foreach ($attendances as $att){
+			$total_avg_late += $att['avg_late'];
+			$total_extra_work += $att['avg_extra_work'];
+		}
+		
+		$this->view->template->trySet('value1',$total_avg_late);
+		$this->view->template->trySet('value2',$total_extra_work);
+		// $attendances = $this->add('xepan\hr\Model_Employee_Attandance');
+		// $attendances->addExpression('avg_late')->set($attendances->dsql()->expr('AVG([0])/60',[$attendances->getElement('late_coming')]));
+		// $attendances->addExpression('avg_extra_work')->set($attendances->dsql()->expr('AVG([0])/60',[$attendances->getElement('extra_work')]));
+		// $attendances->_dsql()->group('employee_id');
      	
-		$this->chart->setType('bar')
-		    		->setModel($attendances,'employee',['avg_late','avg_extra_work'])
-		    		->rotateAxis()
-		    		->setTitle('Employee Avg Late Coming & Extra Work');
+		// $this->chart->setType('bar')
+		//     		->setModel($attendances,'employee',['avg_late','avg_extra_work'])
+		//     		->rotateAxis()
+		//     		->setTitle('Employee Avg Late Coming & Extra Work');
 		
 		return parent::recursiveRender();
 	}
