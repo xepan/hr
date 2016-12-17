8d99f00ab232ca2be83ed69f4ec856562e8ae6f5
vaibhav <vaibhavsharma38@gmail.com>
2016-12-17 15:11:26 +0530

    employee limit checked before insert

--- lib/Model/Employee.php
+++ lib/Model/Employee.php
@@ -75,6 +75,22 @@ class Model_Employee extends \xepan\base\Model_Contact{
 		$this->addHook('beforeSave',[$this,'updateSearchString']);
 		$this->addHook('afterSave',[$this,'updateEmployeeSalary']);
 		$this->addHook('afterSave',[$this,'updateEmployeeLeave']);
+		$this->addHook('beforeInsert',[$this,'checkLimits']);
+	}
+
+	function checkLimits(){
+		$extra_info = $this->app->recall('epan_extra_info_array',false);
+
+        if((isset($extra_info ['specification']['user_limit'])) AND ($extra_info ['specification']['user_limit'] <> 0)){
+        	$emp_count = $this->add('xepan\hr\Model_Employee')->count()->getOne();
+        	
+        	if($emp_count >= $extra_info ['specification']['user_limit']){
+        		throw $this->exception("Sorry ! You cannot add more employees. Your usage limit is over")
+        				->addMoreInfo('Employee Count',$emp_count)
+        				->addMoreInfo('Employee Limit',$extra_info ['specification']['user_limit'])
+        			;
+        	}
+        }
 	}
 
 	function getActiveEmployeeIds(){
