0a1a103bad2867a30d15fdf265de34b82a18a044
dkanojia <dkanojia93@gmail.com>
2017-01-04 20:10:09 +0530

    hr updated .......

--- page/config.php
+++ page/config.php
@@ -2,20 +2,11 @@
 
 namespace xepan\hr;
 
-class page_config extends \xepan\base\Page{
+class page_config extends \xepan\hr\page_configurationsidebar{
 	public $title = "Employee Configuration";
 
 	function init(){
 		parent::init();
-		// $this->app->side_menu->addItem(['Employee Attandance','icon'=>'fa fa-percent'],'xepan_hr_employeeattandance')->setAttr(['title'=>'Employee Attandance']);
-			
-		$this->app->side_menu->addItem(['Pay Slip Layouts','icon'=>'fa fa-th'],'xepan_hr_layouts')->setAttr(['title'=>'Pay Slip Layouts']);
-		$this->app->side_menu->addItem(['Salary Template','icon'=>'fa fa-percent'],'xepan_hr_salarytemplate')->setAttr(['title'=>'Salary Template']);
-		$this->app->side_menu->addItem(['Leave Template','icon'=>'fa fa-percent'],'xepan_hr_leavetemplate')->setAttr(['title'=>'Leave Template']);
-		$this->app->side_menu->addItem(['Official Holidays','icon'=>'fa fa-calendar'],'xepan_hr_officialholiday')->setAttr(['title'=>'Official Holidays']);
-		$this->app->side_menu->addItem(['Working Week Days','icon'=>'fa fa-calendar'],'xepan_hr_workingweekday')->setAttr(['title'=>'Working Week Days']);
-		$this->app->side_menu->addItem(['Misc Config','icon'=>'fa fa-calendar'],'xepan_hr_miscconfig')->setAttr(['title'=>'Misc Config']);
-	
 
 	}
 }
---
+++ page/configurationsidebar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace xepan\hr;
+
+class page_configurationsidebar extends \xepan\base\Page{
+	function init(){
+		parent::init();
+
+		$this->app->side_menu->addItem(['Working Week Days','icon'=>'fa fa-calendar'],'xepan_hr_workingweekday')->setAttr(['title'=>'Working Week Days']);
+		$this->app->side_menu->addItem(['Official Holidays','icon'=>'fa fa-calendar'],'xepan_hr_officialholiday')->setAttr(['title'=>'Official Holidays']);
+		$this->app->side_menu->addItem(['Leave Template','icon'=>'fa fa-file-o'],'xepan_hr_leavetemplate')->setAttr(['title'=>'Leave Template']);
+		$this->app->side_menu->addItem(['Salary Template','icon'=>'fa fa-file-o'],'xepan_hr_salarytemplate')->setAttr(['title'=>'Salary Template']);
+		$this->app->side_menu->addItem(['Misc Config','icon'=>'fa fa-cog fa-spin'],'xepan_hr_miscconfig')->setAttr(['title'=>'Misc Config']);
+		$this->app->side_menu->addItem(['Pay Slip Layouts','icon'=>'fa fa-th'],'xepan_hr_layouts')->setAttr(['title'=>'Pay Slip Layouts']);
+
+	}
+}
