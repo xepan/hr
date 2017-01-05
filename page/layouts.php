01e618d3cf19a08c6520ad1b0501b24e6ad61c9f
dkanojia <dkanojia93@gmail.com>
2017-01-05 14:56:37 +0530

    layouts updated

--- page/layouts.php
+++ page/layouts.php
@@ -2,13 +2,13 @@
 
 namespace xepan\hr;
 
-class page_layouts extends \xepan\hr\page_config{
+class page_layouts extends \xepan\hr\page_configurationsidebar{
 	public $title = "Layouts";
 	function init(){
 		parent::init();
 		
 		
-		/*=========== START PERSON PAYSLIP LAYOUT CONFIG =============================*/
+		/*=========== PAYSLIP LAYOUT CONFIG =============================*/
 
 		$personpayslip_m = $this->add('xepan\base\Model_ConfigJsonModel',
 			[
@@ -58,53 +58,6 @@ class page_layouts extends \xepan\hr\page_config{
 				return $personpayslip_form->js()->univ()->successMessage('Saved')->execute();
 			}	
 		}
-
-		/*=========== End PERSON PAYSLIP LAYOUT CONFIG =============================*/
-
-		// /*=========== EMPLOYEE LIST PAYSLIP LAYOUT CONFIG =============================*/
-
-		// $employeelistpayslip_m = $this->add('xepan\base\Model_ConfigJsonModel',
-		// 	[
-		// 		'fields'=>[
-		// 					'payslip'=>'xepan\base\RichText',
-		// 					],
-		// 			'config_key'=>'EMPLOYEE_LIST_PAYSLIP_LAYOUT',
-		// 			'application'=>'hr'
-		// 	]);
-		// $employeelistpayslip_m->add('xepan\hr\Controller_ACL');
-		// $employeelistpayslip_m->tryLoadAny();
-
-		// $employeelistpayslip_form = $this->add('Form',null,'employees');
-		// $employeelistpayslip_form->setModel($employeelistpayslip_m);
-
-		// $employeelistpayslip_form->getElement('payslip')->set($employeelistpayslip_m['payslip'])->setFieldHint('{$presents},{$paid_leaves},{$unpaid_leaves},{$absents},{$paiddays},{$total_working_days}');
-		
-
-		// $save = $employeelistpayslip_form->addSubmit('Save')->addClass('btn btn-primary');
-		// $reset = $employeelistpayslip_form->addSubmit('Reset Default')->addClass('btn btn-primary');
-
-		// if($employeelistpayslip_form->isSubmitted()){
-		// 	if($employeelistpayslip_form->isClicked($save)){
-		// 		$employeelistpayslip_form->save();
-		// 		$employeelistpayslip_m->app->employee
-		// 	    ->addActivity("Payslip Layout Updated", null/* Related Document ID*/, null /*Related Contact ID*/,null,null,"xepan_hr_layouts")
-		// 		->notifyWhoCan(' ',' ',$employeelistpayslip_m);
-		// 		return $employeelistpayslip_form->js()->univ()->successMessage('Saved')->execute();
-		// 	}
-
-		// 	if($employeelistpayslip_form->isClicked($reset)){
-		// 		$ptemp = file_get_contents(realpath("../vendor/xepan/hr/templates/view/payslip-templates/duplicate-payslip-employee.html"));
-				
-		// 		$employeelistpayslip_m['payslip'] = $ptemp;
-		// 		$employeelistpayslip_m->save();
-		// 		$employeelistpayslip_m->app->employee
-		// 	    ->addActivity("Payslip Layout Updated", null/* Related Document ID*/, null /*Related Contact ID*/,null,null,"xepan_hr_layouts")
-		// 		->notifyWhoCan(' ',' ',$employeelistpayslip_m);			
-		// 		return $employeelistpayslip_form->js()->univ()->successMessage('Saved')->execute();
-		// 	}	
-		// }
-
-		// /*=========== EMPLOYEE LIST PAYSLIP LAYOUT CONFIG =============================*/
 	}
 
 	function defaultTemplate(){
