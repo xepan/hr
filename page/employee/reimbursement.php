60589621298d38b9c82ab121b7202c113eb069c0
dkanojia <dkanojia93@gmail.com>
2017-01-06 18:26:07 +0530

    my reimbursement page updated

--- page/employee/reimbursement.php
+++ page/employee/reimbursement.php
@@ -2,11 +2,10 @@
 
 namespace xepan\hr;
 
-class page_employee_reimbursement extends \xepan\hr\page_employee_myhr{
-	public $title="My Reimbursement";
-	function init(){
-		parent::init();
-
+class page_employee_reimbursement extends \xepan\hr\page_employee_myhr {
+	public $title = "My Reimbursement";
+	
+	function page_index(){
 
 		$crud = $this->add('xepan\hr\CRUD',null,null,['view/employee/reimbursement']);
 
@@ -15,10 +14,23 @@ class page_employee_reimbursement extends \xepan\hr\page_employee_myhr{
 		$model->setOrder('created_at','desc');
 
 		$crud->setModel($model);
-		$crud->addRef('Details',[
-									'view_class'=>"xepan\base\CRUD",
-									'label'=>"Details",
-									'fields'=>['name','date','narration','amount']
-								]);
+
+		$crud->grid->addColumn('expander','Detail');
+		$crud->grid->addPaginator(5);
+		$crud->grid->addQuickSearch(['name']);
 	}
+
+	function page_Detail(){
+		$reimbursement_m = $this->add('xepan\hr\Model_Reimbursement');
+		$reimbursement_m->load($this->app->stickyGET('document_id'));
+
+		$reimbursement_detail_m = $reimbursement_m->ref('Details');
+
+		$crud = $this->add('xepan\hr\CRUD',null,null,['view/employee/reimbursement-detail']);
+		$crud->setModel($reimbursement_detail_m,
+						['name','date','narration','amount'],
+						['name','date','narration','amount']
+					   );
+	}
+
 }
