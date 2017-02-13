548319d21a490ca5e8f94021cd59c94cc33807b5
dkanojia <dkanojia93@gmail.com>
2017-02-13 12:26:50 +0530

    created by id field added in model leave and leave template

--- lib/Model/LeaveTemplate.php
+++ lib/Model/LeaveTemplate.php
@@ -10,6 +10,7 @@ class Model_LeaveTemplate extends \xepan\base\Model_Table{
 	function init(){
 		parent::init();
 
+		$this->hasOne('xepan\hr\Employee','created_by_id')->defaultValue($this->app->employee->id);
 		$this->addField('name');
 
 		$this->hasMany('xepan\hr\LeaveTemplateDetail','leave_template_id');
