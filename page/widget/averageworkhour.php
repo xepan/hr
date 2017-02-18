b995a8d29ca4d90100d844775c9fec6c2f675408
vijay mali <guddavijay@gmail.com>
2017-02-18 12:42:39 +0530

    department average workhour widget done

--- page/widget/averageworkhour.php
+++ page/widget/averageworkhour.php
@@ -11,6 +11,7 @@ class page_widget_averageworkhour extends \xepan\base\Page{
 		$from_date = $this->app->stickyGET('from_date');
 		$to_date = $this->app->stickyGET('to_date');
 
+
 		$attendances = $this->add('xepan\hr\Model_Employee_Attandance');
 		$attendances->addCondition('employee',$x_axis);
 		$attendances->setOrder('fdate','desc');
@@ -19,6 +20,12 @@ class page_widget_averageworkhour extends \xepan\base\Page{
 			$attendances->addCondition('fdate','>=',$from_date);
 			$attendances->addCondition('fdate','<',$this->app->nextDate($to_date));
 		}
+
+		if($_GET['start_date'])
+			$attendances->addCondition('fdate','>=',$_GET['start_date']);
+			
+		if($_GET['end_date'])
+			$attendances->addCondition('fdate','<',$this->app->nextDate($_GET['end_date']));
 
 		$grid = $this->add('xepan\hr\Grid',null,null,['page\widget\averagework']);
 		$grid->setModel($attendances,['employee','fdate','working_hours']);
