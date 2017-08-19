244f534a153cf4c58ad28bab11c5dcbb2d77d27d
vijay mali <guddavijay@gmail.com>
2017-08-19 11:05:41 +0530

    form field class added

--- page/report/employeeattandance.php
+++ page/report/employeeattandance.php
@@ -15,13 +15,13 @@ class page_report_employeeattandance extends \xepan\base\Page{
 		$from_date = $this->app->stickyGET('from_date');
 		$to_date = $this->app->stickyGET('to_date');
 		$form = $this->add('Form',null,null,['form/empty']);
-		$date = $form->addField('DateRangePicker','date_range');
+		$date = $form->addField('DateRangePicker','date_range')->addClass('xepan-push');
 		$set_date = $this->app->today." to ".$this->app->today;
 		if($from_date){
 			$set_date = $from_date." to ".$to_date;
 			$date->set($set_date);	
 		}
-		$emp_field = $form->addField('xepan\base\Basic','employee');
+		$emp_field = $form->addField('xepan\base\Basic','employee')->addClass('xepan-push');
 		$emp_field->setModel('xepan\hr\Model_Employee')->addCondition('status','Active');
 		
 		$attandance_m = $this->add('xepan\hr\Model_Employee_Attandance',['from_date'=>$_GET['from_date'],'to_date'=>$_GET['to_date']]);
