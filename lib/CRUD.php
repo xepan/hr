211a580412dc17706298fdff8d77a0660c49f14d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-08-21 11:13:04 +0530

    working on better approach to code

--- lib/CRUD.php
+++ lib/CRUD.php
@@ -7,6 +7,7 @@ class CRUD extends \xepan\base\CRUD {
 
 	function noAttachment(){
 		$this->grid->removeColumn('attachement_icon');
+		$this->grid->removeColumn('attachment_icon');
 	}
 	
 	function setModel($model,$grid_fields=null,$form_fields=null){
@@ -17,5 +18,12 @@ class CRUD extends \xepan\base\CRUD {
 			$this->add('xepan\hr\Controller_ACL',['status_color'=>$this->status_color]);
 		}
 		return $m;
+	}
+
+	function recursiveRender(){
+		if($this->grid->hasColumn('edit')){
+			$this->grid->addOrder()->move('edit','before','delete')->now();
+		}
+		return parent::recursiveRender();
 	}
 }
--- page/department.php
+++ page/department.php
@@ -20,18 +20,18 @@ class page_department extends \xepan\base\Page {
 		$department=$this->add('xepan\hr\Model_Department');
 		$department->add('xepan\hr\Controller_SideBarStatusFilter');
 		
-		if($status = $this->app->stickyGET('status'))
-			$department->addCondition('status',$status);
-
 
 		$department->setOrder('production_level','asc');
-		$crud=$this->add('xepan\hr\CRUD',null,null,['view/department/department-grid']);
+		// $crud=$this->add('xepan\hr\CRUD',null,null,['view/department/department-grid']);
+		$crud=$this->add('xepan\hr\CRUD');
 		$crud->grid->addPaginator(50);
+
+		
 		
 		if(!$crud->isEditing())
 			$crud->grid->template->trySet('dept-url',$this->app->url('xepan_hr_structurechart'));
 
-		$crud->setModel($department);
+		$crud->setModel($department,['name','production_level','is_outsourced','simultaneous_no_process_allowed','posts_count','employee_count']);
 		$crud->add('xepan\base\Controller_MultiDelete');
 
 		if($crud->form->model['is_system']){
@@ -40,18 +40,25 @@ class page_department extends \xepan\base\Page {
 
 		$crud->grid->addHook('formatRow',function($g){
 			if($g->model['is_system']) {
-				$g->current_row_html['edit']  = '<span class="fa-stack table-link"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-pencil fa-stack-1x fa-inverse"></i></span>';
-				$g->current_row_html['delete']= '<span class="table-link fa-stack"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-trash-o fa-stack-1x fa-inverse"></i></span>';
+				$g->row_edit=false;
+				$g->row_delete=false;
 				$g->current_row_html['action']='';
+			}else{
+				$g->row_edit=true;
+				$g->row_delete=true;
 			}
 			
 		});
 
 		$f=$crud->grid->addQuickSearch(['name']);
 
-		if(!$crud->isEditing()){
-			$crud->grid->js('click')->_selector('.do-view-department-post')->univ()->frameURL('Department Post',[$this->api->url('xepan_hr_post'),'department_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
-			$crud->grid->js('click')->_selector('.do-view-department-employee')->univ()->frameURL('Department Employee',[$this->api->url('xepan_hr_employee'),'department_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id'),'status'=>'']);
-		}
+		$crud->grid->addFormatter('posts_count','template')->setTemplate('<a href="#pc" class="do-view-department-post">{$posts_count}</a>','posts_count');
+		$crud->grid->addFormatter('employee_count','template')->setTemplate('<a href="#ec" class="do-view-department-employee">{$employee_count}</a>','employee_count');
+
+		$crud->grid->js('click')->_selector('.do-view-department-post')->univ()->frameURL('Department Post',[$this->api->url('xepan_hr_post'),'department_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
+		$crud->grid->js('click')->_selector('.do-view-department-employee')->univ()->frameURL('Department Employee',[$this->api->url('xepan_hr_employee'),'department_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id'),'status'=>'']);
+
+		$crud->grid->addSno();
+		$crud->noAttachment();
 	}
 }
