267ef8c4885e19c809842d50ec379b12453378e4
sinharakesh <rksinha.btech@gmail.com>
2017-09-29 13:14:30 +0530

    leave template crud currected

--- lib/Model/Leave.php
+++ lib/Model/Leave.php
@@ -12,7 +12,7 @@ class Model_Leave extends \xepan\base\Model_Table{
 	function init(){
 		parent::init();
 
-		$this->hasOne('xepan\hr\Employee','created_by_id')->defaultValue($this->app->employee->id);
+		$this->hasOne('xepan\hr\Employee','created_by_id')->defaultValue($this->app->employee->id)->system(true);
 	
 		$this->addField('name');
 		$this->addField('is_yearly_carried_forward')->type('boolean')->defaultValue(false);
