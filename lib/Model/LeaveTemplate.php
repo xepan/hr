9137c5520b0a23c8015c61d54b20003727933a74
sinharakesh <rksinha.btech@gmail.com>
2017-09-29 16:23:12 +0530

    leave template beforeDelete details deleted

--- lib/Model/LeaveTemplate.php
+++ lib/Model/LeaveTemplate.php
@@ -14,5 +14,19 @@ class Model_LeaveTemplate extends \xepan\base\Model_Table{
 		$this->addField('name');
 		
 		$this->hasMany('xepan\hr\LeaveTemplateDetail','leave_template_id');
+		$this->addHook('beforeDelete',$this);
+	}
+
+	function beforeDelete(){
+		// first check leave is apply on post or employee not
+		$post = $this->add('xepan\hr\Model_Post');
+		$post->addCondition('leave_template_id',$this->id);
+		if($post->count()->getOne()){
+			throw new \Exception("first remove leave template from post");
+		}
+
+		$model = $this->add('xepan\hr\Model_LeaveTemplateDetail');
+		$model->addCondition('leave_template_id',$this->id)
+				->deleteAll();
 	}
 }
