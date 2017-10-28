79a0f8ac032d2e82905cd22702e2b6fe418f0537
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-10-28 23:01:50 +0530

    may be no app->employee is set due to HR application not installed

--- lib/Model/Post/Email/MyEmails.php
+++ lib/Model/Post/Email/MyEmails.php
@@ -9,7 +9,8 @@ class Model_Post_Email_MyEmails extends \xepan\communication\Model_Communication
 		$this->addCondition('is_active',true);
 		$ass_j=$this->join('post_email_association.emailsetting_id');
 		$ass_j->addField('post_id');
-		$this->addCondition('post_id',$this->app->employee['post_id']);
+		if(isset($this->app->employee))
+			$this->addCondition('post_id',$this->app->employee['post_id']);
 
 		$this->addExpression('post_email')->set(function($m,$q){
 			return $q->getField('email_username');
