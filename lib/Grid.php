c19eda117514b6a29221c51417c49bac1f881ffb
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-05-08 11:42:04 +0530

    hr grid is also now populates acl id pass_acl is setted false.. default mute

--- lib/Grid.php
+++ lib/Grid.php
@@ -11,4 +11,26 @@
 
 namespace xepan\hr;
 
-class Grid extends \xepan\base\Grid{}
+class Grid extends \xepan\base\Grid{
+
+	public $pass_acl=true;
+	public $actionsWithoutACL=false;
+	public $status_color=[];
+	public $permissive_acl=false;
+	public $show_spot_acl=true;
+
+	function setModel($model,$grid_fields=null){
+
+		$m = parent::setModel($model,$grid_fields);
+		
+		if(($m instanceof \xepan\base\Model_Table) && !$this->pass_acl){
+			if($this->actionsWithoutACL || isset($this->app->actionsWithoutACL)){
+				$this->add('xepan\hr\Controller_Action',['status_color'=>$this->status_color,'permissive_acl'=>$this->permissive_acl,'show_spot_acl'=>$this->show_spot_acl,'actionsWithoutACL'=>$this->actionsWithoutACL]);
+			}else{
+				$this->add('xepan\hr\Controller_ACL',['status_color'=>$this->status_color,'permissive_acl'=>$this->permissive_acl,'show_spot_acl'=>$this->show_spot_acl,'actionsWithoutACL'=>$this->actionsWithoutACL]);
+			}
+		}
+		return $m;
+	}
+
+}
