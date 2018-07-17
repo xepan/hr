9b5ebabdbb94f9a03d7ae9456d5a5d46afac975c
monikapokra <monikagupta495@gmail.com>
2018-07-17 15:37:54 +0530

    form beautified

--- page/deduction.php
+++ page/deduction.php
@@ -9,6 +9,21 @@ class page_deduction extends \xepan\base\Page{
 		parent::init();
 
 		$crud = $this->add('xepan\hr\CRUD');
+		if($crud->form){
+			$form = $crud->form;
+			$form->add('xepan\base\Controller_FLC')
+				->showLables(true)
+				->addContentSpot()
+				->layout([
+						'employee_id~Employee Name'=>'Deduction~c1~12',
+						'created_at'=>'c2~6',
+						'name~Reason'=>'c3~6',
+						'amount'=>'c4~6',
+						'received_amount'=>'c5~6',
+						'narration'=>'c6~12',
+						'FormButtons~&nbsp'=>'c7~4'
+					]);
+		}
 		$model = $this->add('xepan\hr\Model_Deduction');
 		$model->setOrder('created_at','desc');
 		$crud->setModel($model,
@@ -18,5 +33,6 @@ class page_deduction extends \xepan\base\Page{
 
 		$crud->grid->addSno();
 		$crud->add('xepan\base\Controller_MultiDelete');
+		
 	}
 }
